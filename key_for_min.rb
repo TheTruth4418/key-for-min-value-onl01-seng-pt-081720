f4a2666f0cd7622fe353ab2ec6aec5bc4d3fa00a
William West <wwest466@gmail.com>
2020-08-25 23:48:16 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,15 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+arr2 = []
+  name_hash.map do |x,y|
+    arr2 << y
+  end
 
+arr3 = arr2.sort
+
+key = name_hash.find do |x,y|
+  y == arr3[0]
+end
+return key[0]
 end
